4a034c1deab246fac89c6a305acdd122f457620d
Henrique Rodrigues <henrique.hfr2006@gmail.com>
2024-08-20 23:23:35 -0300

    atualização

---
+++ apps/galeria/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+from apps.galeria.models import Fotografia
+
+class FotografiaForms(forms.ModelForm):
+  class Meta:
+    model = Fotografia
+    exclude=[
+      'publicada',
+    ]
+    labels={
+      'descricao': 'Descrição',
+      'data_fotografia': 'Data de registro',
+      'user_id': 'Usuário',
+    }
+    
+    widgets = {
+      'nome': forms.TextInput(attrs={'class': 'form-control'}),
+      'legenda': forms.TextInput(attrs={'class': 'form-control'}),
+      'categoria': forms.Select(attrs={'class': 'form-control'}),
+      'descricao': forms.Textarea(attrs={'class': 'form-control'}),
+      'foto': forms.FileInput(attrs={'class': 'form-control'}),
+      'data_fotografia': forms.DateInput(
+        format='%d/%M/%Y',
+        attrs={
+          'type': 'date',
+          'class': 'form-control'
+      }),
+      'user_id': forms.Select(attrs={'class': 'form-control'}),
+    }
